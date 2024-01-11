5057085407c24d1f882fb969695f56e88f684165
nightblade9 <nightbladecodes@gmail.com>
2024-01-10 23:27:59 -0500

    ScreenReaderStatus label: done.
    Displays status, speaks on state-change (using built-in TTS).

--- demo/addons/screen_reader/NvdaTts.gd
+++ demo/addons/screen_reader/NvdaTts.gd
@@ -4,11 +4,13 @@ extends Node
 var voice:String
 
 func _init():
-	var voices = DisplayServer.tts_get_voices_for_language("en")
-	self.voice = voices[0] # pick the first. Just 'cuz.
+	_load_voices()
 
 # Say something! If we're already talking, stop.
 func say(text:String) -> void:
+	if self.voice == null:
+		_load_voices()
+		
 	self.stop()
 	if is_using_nvda():
 		NVDA.speak_text(text)
@@ -24,3 +26,9 @@ func stop():
 
 func is_using_nvda():
 	return NVDA.is_running()
+
+func _load_voices():
+	var voices = DisplayServer.tts_get_voices_for_language("en")
+	
+	if len(voices) > 0:
+		self.voice = voices[0] # pick the first. Just 'cuz.
---
+++ demo/addons/screen_reader/ScreenReaderStatusLabel.gd
@@ -0,0 +1,30 @@
+extends Label
+
+var speak_on_state_change:bool = true
+var _is_enabled_last_check:bool = false
+
+func _ready():
+	NvdaTts.say(self.text)
+
+func _process(_delta):
+	var is_enabled = NVDA.is_running()
+	var status_text = "enabled" if is_enabled else "disabled"
+	self.text = "NVDA integration %s" % status_text
+	_check_for_state_changes(is_enabled)
+	_is_enabled_last_check = is_enabled
+
+func _check_for_state_changes(is_enabled:bool) -> void:
+	if not speak_on_state_change:
+		return
+	
+	var say_status = false
+	if is_enabled and not _is_enabled_last_check:
+		# Enabled
+		say_status = true
+	elif not is_enabled and _is_enabled_last_check:
+		# Disabled
+		say_status = true
+	
+	if say_status:
+		# NVDA doesn't say this on boot-up. Sad. Use built-in TTS.
+		DisplayServer.tts_speak(self.text, NvdaTts.voice)
