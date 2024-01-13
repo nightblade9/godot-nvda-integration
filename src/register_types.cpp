decec699e6318cb8142965fcc2ead36300ea168b
nightblade9 <nightbladecodes@gmail.com>
2024-01-12 20:57:52 -0500

    Whitespace fix

--- src/register_types.cpp
+++ src/register_types.cpp
@@ -25,14 +25,14 @@ void uninitialize_nvda_module(ModuleInitializationLevel p_level) {
 }
 
 extern "C" {
-// Initialization.
-GDExtensionBool GDE_EXPORT nvda_library_init(GDExtensionInterfaceGetProcAddress p_get_proc_address, GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization) {
-	godot::GDExtensionBinding::InitObject init_obj(p_get_proc_address, p_library, r_initialization);
+	// Initialization.
+	GDExtensionBool GDE_EXPORT nvda_library_init(GDExtensionInterfaceGetProcAddress p_get_proc_address, GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization) {
+		godot::GDExtensionBinding::InitObject init_obj(p_get_proc_address, p_library, r_initialization);
 
-	init_obj.register_initializer(initialize_nvda_module);
-	init_obj.register_terminator(uninitialize_nvda_module);
-	init_obj.set_minimum_library_initialization_level(MODULE_INITIALIZATION_LEVEL_SCENE);
+		init_obj.register_initializer(initialize_nvda_module);
+		init_obj.register_terminator(uninitialize_nvda_module);
+		init_obj.set_minimum_library_initialization_level(MODULE_INITIALIZATION_LEVEL_SCENE);
 
-	return init_obj.init();
-}
+		return init_obj.init();
+	}
 }
